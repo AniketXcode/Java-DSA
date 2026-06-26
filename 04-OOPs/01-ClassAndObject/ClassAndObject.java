f07ea0382528ed0a609c579211f084f81f86ae9b
Aniket <aniketxcode@gmail.com>
2026-06-26 15:06:26 +0530

    ClassAndObject

---
+++ 04-OOPs/01-ClassAndObject/ClassAndObject.java
@@ -0,0 +1,16 @@
+public class ClassAndObject {
+ public static void main(String[] args) {
+
+        // Create an object of the class
+        ClassAndObject obj = new ClassAndObject();
+
+        // Call the method using the object
+        obj.displayMessage();
+    }
+
+    // Method to display a message
+    public void displayMessage() {
+        System.out.println("Hello, this is a message from the ClassAndObject class!");
+    }
+ }    
+
